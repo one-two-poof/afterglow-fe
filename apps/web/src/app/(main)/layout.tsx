02134317a08b0d13cd5c7779abc18cc4dc42c745
SanginJeong <sanginjeong07@gmail.com>
2026-08-06 16:46:46 +0900

    refactor: Layout 구조 재설정

--- apps/web/src/app/(main)/layout.tsx
+++ apps/web/src/app/(main)/layout.tsx
@@ -1,13 +1,13 @@
-import { Header } from "@/components";
-import Footer from "@/components/Footer";
+import { Nav } from "@/components";
 import { ReactNode } from "react";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex min-h-dvh flex-col">
-      <Header />
-      <main className="h-[1000px] flex-1">{children}</main>
-      <Footer />
+    <div className="flex min-h-dvh justify-center bg-neutral-100">
+      <div className="flex min-h-dvh w-full max-w-4xl flex-col bg-surface shadow-sm">
+        <main className="flex-1">{children}</main>
+        <Nav />
+      </div>
     </div>
   );
 };
